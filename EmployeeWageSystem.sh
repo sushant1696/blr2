cddf4afcddd7726a1f8be8f1150844b6e611f230
sushant1696 <shaan160196@gmail.com>
2020-02-12 19:15:27 +0530

    [sushanta] employe wage attendance

---
+++ EmployeeWageSystem.sh
@@ -0,0 +1,12 @@
+echo "******** wellcome to EmployeeWageSystem ********"
+CheckPresentAbsent(){
+rand=$(($RANDOM%2))
+if(($rand<1))
+then
+echo "Employee is absent"
+else
+echo "Employee is present"
+fi
+echo "$rand"
+}
+CheckPresentAbsent
