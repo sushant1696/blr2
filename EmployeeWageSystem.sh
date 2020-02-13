95e144a98ec0e37a44f6fb8494784cc52218aace
sushant1696 <shaan160196@gmail.com>
2020-02-13 15:30:25 +0530

    [sushant]Refectore.modified the user case1 program

--- EmployeeWageSystem.sh
+++ EmployeeWageSystem.sh
@@ -1,2 +1,11 @@
-
-helo
+CheckPresentAbsent(){
+rand=$(($RANDOM%2))
+if(($rand<1))
+then
+echo "Sorry the Employee is absent"
+else
+echo "Yes Employee is present"
+fi
+echo "$rand"
+}
+CheckPresentAbsent
