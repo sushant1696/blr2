601cf8c8c8bba324505d61ce31c1a22a6fcd6c44
sushant1696 <shaan160196@gmail.com>
2020-02-13 16:53:47 +0530

    [sushant]Refectore.modified use_case3

--- EmployeeWageSystem.sh
+++ EmployeeWageSystem.sh
@@ -1,12 +1,12 @@
 
 PartTimeEmployee(){
-	echo "enter part time Employee name  "
+	echo "enter Employee name "
 	read name
 	echo "Part time Employee name is :$name"
 	echo "enter the per hour wage"
 	read wage
 	totalhour=4
 	PertTimeWage=$(($wage*$totalhour))
-	echo "The Employee total wage is = $PertTimeWage"
+	echo "Employee total wage is = $PertTimeWage"
 }
 PartTimeEmployee
