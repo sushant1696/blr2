b6cad92b7d1eef5997e0d5ba41118239c178832d
sushant1696 <shaan160196@gmail.com>
2020-02-13 13:28:25 +0530

    [Sushant]Refector.modified the Part time employee

--- EmployeeWageSystem.sh
+++ EmployeeWageSystem.sh
@@ -1,12 +1,12 @@
 
 PartTimeEmployee(){
-	echo "enter a part time Employee "
+	echo "enter any part time Employee= "
 	read name
 	echo "Part time Employee name is :$name"
 	echo "enter the per hour wage"
 	read wage
 	totalhour=4
 	PertTimeWage=$(($wage*$totalhour))
-	echo "Total price of part time employee is =$PertTimeWage"
+	echo "Total wage of part time employee is = $PertTimeWage"
 }
 PartTimeEmployee
