ab3cedac3d4a3ef359c070f0865b00bda2f63f0a
sushant1696 <shaan160196@gmail.com>
2020-02-13 12:12:01 +0530

    [Sushant]Refector.modified the use_case2

--- EmployeeWageSystem.sh
+++ EmployeeWageSystem.sh
@@ -1,12 +1,7 @@
-
-CheckPresentAbsent(){
-rand=$(($RANDOM%2))
-if(($rand<1))
-then
-echo "Sorry Employee is absent"
-else
-echo "Employee is present"
-fi
-echo "$rand"
+DailyEmployeeWage(){
+	PerHourWage=20
+	TotalHour=8
+	EmployeeWagePerDay=$(($PerHourWage*$TotalHour))
+	echo "Employee wage in a day is=$EmployeeWagePerDay"
 }
-CheckPresentAbsent
+DailyEmployeeWage
