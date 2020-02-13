5d48642a9b3261f14b302651ff7d4e903c657a8c
sushant1696 <shaan160196@gmail.com>
2020-02-13 15:52:20 +0530

    [sushant]Refectote.modified the usecase1

--- EmployeeWageSystem.sh
+++ EmployeeWageSystem.sh
@@ -1,7 +1,12 @@
-DailyEmployeeWage(){
-	PerHourWage=20
-	TotalHour=8
-	EmployeeWagePerDay=$(($PerHourWage*$TotalHour))
-	echo "Employee wage in a day is=$EmployeeWagePerDay"
+
+CheckPresentAbsent(){
+rand=$(($RANDOM%2))
+if(($rand<1))
+then
+echo "Sorry! Employee is absent"
+else
+echo "Employee is present"
+fi
+echo "$rand"
 }
-DailyEmployeeWage
+CheckPresentAbsent
